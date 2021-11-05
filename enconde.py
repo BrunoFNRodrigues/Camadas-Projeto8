0df4f9bc64f920e4ee6b3683ba5c9e1978873deb
keiyanishio <keiyan@al.insper.edu.br>
2021-11-05 17:19:05 -0300

    messagem inspiradora

--- enconde.py
+++ enconde.py
@@ -11,11 +11,12 @@ import peakutils
 import time 
 import soundfile as sf
 from suaBibSignal import *
+from funcoes_LPF import *
 
 audio, samplerate = sf.read('audio/ac130.wav')
 print(samplerate)
 
-audio = audio*10e5
+audio = audio*(1/0.15)
 
 freqDeAmostragem = 44100
 duracao = 2
@@ -28,7 +29,9 @@ numPontos = numAmostras
 
 t = np.linspace(inicio,fim,numPontos)
 # plot do gravico  áudio vs tempo!
+plt.title('Audio puro')
 plt.plot(t, audio[:88200,0])
+
 
 signal= signalMeu()
 fs = freqDeAmostragem
@@ -37,3 +40,38 @@ plt.figure("F(y)")
 plt.plot(xf,yf)
 plt.grid()
 plt.title('Fourier audio')
+plt.show()
+
+filtrado = filtro(audio[:88200,0], samplerate, 14000)
+#sd.play(filtrado, samplerate)
+
+t = np.linspace(inicio,fim,numPontos)
+# plot do gravico  áudio vs tempo!
+plt.plot(t, filtrado)
+plt.title('Audio filtrado')
+plt.show()
+
+
+xf, yf = signal.calcFFT(filtrado, fs)
+plt.figure("F(y)")
+plt.plot(xf,yf)
+plt.grid()
+plt.title('Fourier audio filtrado')
+plt.show()
+
+
+y, x = signal.generateSin(14000, 1, duracao, freqDeAmostragem)
+
+X=x+filtrado*x
+#sd.play(X)
+
+xf, yf = signal.calcFFT(X, fs)
+plt.figure("F(y)")
+plt.plot(xf,yf)
+plt.grid()
+plt.title('Fourier audio modulado')
+plt.show()
+
+sf.write('teste.wav', X, samplerate)
+
+
